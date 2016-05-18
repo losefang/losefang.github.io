5576b6ca70988166fce32cc1d1593c4fe90ae476
losefang <310896827@qq.com>
2016-05-18 10:40:01 +0800

    JAVAscrip练习

---
+++ 11-4/hello.js
@@ -0,0 +1,41 @@
+// alert("Hi");
+
+
+// dfdfdf
+
+// dfdfd
+
+// dfdfd
+
+
+
+
+// var 性别 = '女';
+// alert(性别);
+
+var age = 90;
+// alert(20 == '20');
+
+// {
+//     alert(age + 3);
+//     alert("13" + "广告");
+// }
+
+if (age > 40) {
+    if (age >= 90) { alert("声色张扬"); } else {
+        alert("无所谓的模样");
+    }
+} else {
+    alert("人生却不过百年");
+}
+
+for (var i = 0; i < 100; i++) {
+    console.log(i)
+        // 在控制台输出信息
+}
+
+function sayHello(name) {
+    console.log(name + ", HI");
+}
+
+sayHello("方橙子");
